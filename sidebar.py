9c88f3eabbcb40bdf625c1374e5252ba73afff75
Reski Rukmantiyo <reski.rukmantio@lintasarta.co.id>
2024-07-27 03:57:26 +0000

    update for sidebar and gpus data

--- sidebar.py
+++ sidebar.py
@@ -2,9 +2,8 @@ import streamlit as st
 
 def sidebar():
     with st.sidebar:
-        st.image("cutie.png", use_column_width=True)
-        st.title("About Token Tally")
-        st.info("Select your desired base model, parameters, and configuration to get an estimate of the required GPU memory and model size. Do contribute: https://github.com/adarshxs/TokenTally")
+        st.title("About Token Calculator")
+        st.info("Select your desired base model, parameters, and configuration to get an estimate of the required GPU memory and model size. https://github.com/reski-rukmantiyo/llm_token_calculator")
         # Add a Products section with radio buttons to select a page
         product_options = ["Overview", "LLM Cost Tool", "Transformer Memory Tool", "LLM Model Recommendation"]
         selected_product = st.radio("Products", product_options)
